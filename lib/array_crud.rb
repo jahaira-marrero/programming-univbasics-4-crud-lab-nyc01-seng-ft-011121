1554b66f0459ca2ec6b05cb633fa680743ed11c7
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-21 18:45:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   array = ["apples", "bananas", "cantelope", "dates"]
+   array << "eggplant" 
 end
 
 def add_element_to_start_of_array(array, element)
